d35b99380b1583d163611119bac4b21918e0cf20
p4yl0ad <57832389+p4yl0ad@users.noreply.github.com>
2020-11-06 16:24:26 +0100

    Create domain_machines.ps1

---
+++ domain_machines.ps1
@@ -0,0 +1,17 @@
+$domainObj = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()
+$PDC = ($domainObj.PdcRoleOwner).Name
+$SearchString = "LDAP://" + $PDC + "/"
+$DistinguishedName = "DC=$($domainObj.Name.Replace('.', ',DC='))"
+$SearchString += $DistinguishedName
+$Searcher = New-Object System.DirectoryServices.DirectorySearcher([ADSI]$SearchString)
+$objDomain = New-Object System.DirectoryServices.DirectoryEntry
+$Searcher.SearchRoot = $objDomain
+
+
+$Searcher.filter="sAMAccountType=805306369" #machines 
+$Result = $Searcher.FindAll()
+
+Foreach($obj in $Result)
+{
+        $obj
+}
