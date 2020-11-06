c89479b4da47011f1c96af2ec5bb8a9a72f2d4c5
p4yl0ad <57832389+p4yl0ad@users.noreply.github.com>
2020-11-06 16:24:54 +0100

    Create os_filter.ps1

---
+++ os_filter.ps1
@@ -0,0 +1,18 @@
+$domainObj = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()
+$PDC = ($domainObj.PdcRoleOwner).Name
+$SearchString = "LDAP://" + $PDC + "/"
+$DistinguishedName = "DC=$($domainObj.Name.Replace('.', ',DC='))"
+$SearchString += $DistinguishedName
+$Searcher = New-Object System.DirectoryServices.DirectorySearcher([ADSI]$SearchString)
+$objDomain = New-Object System.DirectoryServices.DirectoryEntry
+$Searcher.SearchRoot = $objDomain
+
+
+$Searcher.filter="sAMAccountType=805306369" 
+$Searcher.filter="operatingsystem=*windows 10*"
+$Result = $Searcher.FindAll()
+
+Foreach($obj in $Result)
+{
+        $obj
+}
