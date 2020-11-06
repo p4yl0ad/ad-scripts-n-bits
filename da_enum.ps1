c59a0588c2eda35cfe3d8b20fe64612e95a192b4
p4yl0ad <57832389+p4yl0ad@users.noreply.github.com>
2020-11-06 16:23:21 +0100

    Create da_enum.ps1

---
+++ da_enum.ps1
@@ -0,0 +1,16 @@
+$domainObj = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()
+$PDC = ($domainObj.PdcRoleOwner).Name
+$SearchString = "LDAP://" + $PDC + "/"
+$DistinguishedName = "DC=$($domainObj.Name.Replace('.', ',DC='))"
+$SearchString += $DistinguishedName
+$Searcher = New-Object System.DirectoryServices.DirectorySearcher([ADSI]$SearchString)
+$objDomain = New-Object System.DirectoryServices.DirectoryEntry
+$Searcher.SearchRoot = $objDomain
+
+$Searcher.filter="memberof=CN=Domain Admins,CN=Users,DC=corp,DC=com"
+$Result = $Searcher.FindAll()
+
+Foreach($obj in $Result)
+{
+        $obj
+}
